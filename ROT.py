2825264ebe5e0943017dd518d50df28e94aa0314
S03HT3T <52712554+S03HT3T@users.noreply.github.com>
2020-06-16 20:27:33 +0630

    Add files via upload

---
+++ ROT.py
@@ -0,0 +1,35 @@
+Purple="\033[0;35m"
+c = "\033[036m"
+Red="\033[0;31m"
+BBlue="\033[1;34m"
+Green="\033[0;32m"
+
+print("		{}##########################".format(Purple))
+print("		#    Caesar Decryptor    #")
+print("		#                        #")
+print("		#                        #")
+print("		#            By SO3HT3T  #")
+print("		##########################\n")
+
+
+words = input("{}ennter your string :: {}".format(c,Green))
+final = ""
+for i in range(1,26):
+	for word in words:
+		if word.islower():
+			rot = ord(word)+i
+			if rot > 122:
+				rot = rot - 26
+			elif rot < 97:
+				rot = rot + 26
+			final += chr(rot)
+		elif word.isupper():
+			rot = ord(word)+i
+			if rot > 90:
+				rot = rot - 26
+			elif rot < 65:
+				rot = rot + 26
+			final += chr(rot)
+	print("ROT "+ str(i) + " :: "+final)
+	final = ""
+
